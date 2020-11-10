604ba31edbede2c1017abc48e1a828d6511c8a74
vincentbevia <vincent.bevia@multisafepay.com>
2020-11-10 17:34:31 +0100

    init commit, LISKOV principle

---
+++ LISKOV/src/com/corebaseit/bevia/Main.java
@@ -0,0 +1,52 @@
+package com.corebaseit.bevia;
+
+public class Main {
+
+    public static void main(String[] args) {
+        /*The Liskov Principle is summarizes by Robert C. Martin like so:
+         Subtypes must be substitutable for their base types.
+
+         Lets say ClassA is my base type and ClassB is my subtype in this example!
+
+         Substitutability is a principle in object-oriented programming introduced
+         by Barbara Liskov in a 1987 conference keynote stating that...
+
+         ...if classB is a subclass of classA, then wherever A is expected, B can be used instead
+         because B is going to contain all of A + more of its own stuff (extends)! */
+
+        //ClassA a = new ClassA();
+        ClassA a = new ClassB();
+        a.superMethod(new ClassB());
+        a.superPrint();
+
+        //Belongs to classB only
+        ClassB a_ = new ClassB();
+        a_.myMethod();
+
+    }
+}
+
+class ClassA {
+
+    public void superMethod(Object o) {
+        if (o instanceof ClassB) {
+            ((ClassB) o).superMethod();
+        }
+    }
+
+    public void superPrint() {
+        System.out.println("superclass");
+    }
+}
+
+class ClassB extends ClassA {
+
+    public void superMethod() {
+        System.out.println("object class B");
+    }
+
+    public void myMethod() {
+        System.out.println("this belong to classB alone!");
+    }
+
+}
